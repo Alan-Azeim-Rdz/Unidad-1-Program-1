c85767aa5e2f9be805abc2dd1dec97e8405e03cd
RODRIGUEZ VILLANUEVA ALAN AZEIM <I23050359@monclova.tecnm.mx>
2024-02-11 15:41:30 -0600

    Agregar archivos de proyecto.

---
+++ Actividad1/Program.cs
@@ -0,0 +1,24 @@
+﻿class operacion
+{
+    static void Main(string[] arg)
+    {
+        int qp = int.Parse(Console.ReadLine());
+        if (qp == 1 )
+        {
+            Console.WriteLine("Enter the first number to be added ");
+            double n1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the following number ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            double Result1 = n1 + n2;
+            Console.WriteLine("the result is " + Result1);
+        }
+
+        Console.ReadKey();
+
+
+    }
+
+}
