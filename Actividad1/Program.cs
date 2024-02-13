e70dcf00336da3a5aded4b8f5673dcd89f9eed8a
RODRIGUEZ VILLANUEVA ALAN AZEIM <I23050359@monclova.tecnm.mx>
2024-02-12 18:03:54 -0600

    se añadiieron nuevas formulas  a la calculadora

--- Actividad1/Program.cs
+++ Actividad1/Program.cs
@@ -2,7 +2,11 @@
 {
     static void Main(string[] arg)
     {
-        int qp = int.Parse(Console.ReadLine());
+        Console.WriteLine("What operation you wish to perform? \n (1) Sddition  (2) Subtraction  (3) Multiplication  (4) Divicion  (5) Exit");
+        
+        string reply = Console.ReadLine();
+        Convert.ToInt32(reply);
+        int qp = int.Parse(reply);
         if (qp == 1 )
         {
             Console.WriteLine("Enter the first number to be added ");
@@ -14,6 +18,46 @@
 
             double Result1 = n1 + n2;
             Console.WriteLine("the result is " + Result1);
+        }
+        else if (qp == 2 )
+        {
+            Console.WriteLine("Enter the first number to be added ");
+            double n1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the following number ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            double Result1 = n1 - n2;
+            Console.WriteLine("the result is " + Result1);
+        }
+        else if(qp == 3 )
+        {
+            Console.WriteLine("Enter the first number to be added ");
+            double n1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the following number ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            double Result1 = n1 * n2;
+            Console.WriteLine("the result is " + Result1);
+        }
+        else if(qp == 4)
+        {
+            Console.WriteLine("Enter the first number to be added ");
+            double n1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the following number ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            double Result1 = n1 / n2;
+            Console.WriteLine("the result is " + Result1);
+        }
+        else
+        {
+            Console.WriteLine("Goodbye ");
         }
 
         Console.ReadKey();
