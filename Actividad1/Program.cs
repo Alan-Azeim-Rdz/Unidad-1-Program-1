6458b687fde6fda74c2a94e854f029fb07118e3b
RODRIGUEZ VILLANUEVA ALAN AZEIM <I23050359@monclova.tecnm.mx>
2024-02-12 22:40:00 -0600

    se le a agregado una actividad para que no se acepten letras en cuanto a las operaciones y se le a dado la isntrucion de no hacerninguna operacion si no es con un numero

--- Actividad1/Program.cs
+++ Actividad1/Program.cs
@@ -2,55 +2,128 @@
 {
     static void Main(string[] arg)
     {
-        Console.WriteLine("What operation you wish to perform? \n (1) Sddition  (2) Subtraction  (3) Multiplication  (4) Divicion  (5) Exit");
+        string reply;
+        int qp;
+        double n1, n2;
+
+        do
+        {
+            Console.WriteLine("What operation you wish to perform? \n (1) Sddition  (2) Subtraction  (3) Multiplication  (4) Divicion  (5...) Exit");
+            reply = Console.ReadLine();
+        }
+        while (!int.TryParse(reply, out qp));
         
-        string reply = Console.ReadLine();
+  
         Convert.ToInt32(reply);
-        int qp = int.Parse(reply);
+        qp = int.Parse(reply);
+
+
         if (qp == 1 )
         {
-            Console.WriteLine("Enter the first number to be added ");
-            double n1 = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter the first number to be added ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n1 = double.Parse(reply);
 
 
-            Console.WriteLine("Enter the following number ");
-            double n2 = double.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Enter the following number ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n2 = double.Parse(reply);
 
             double Result1 = n1 + n2;
             Console.WriteLine("the result is " + Result1);
         }
+
+
         else if (qp == 2 )
         {
-            Console.WriteLine("Enter the first number to be added ");
-            double n1 = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter the first number to be added ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n1 = double.Parse(reply);
 
 
-            Console.WriteLine("Enter the following number ");
-            double n2 = double.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Enter the following number ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n2 = double.Parse(reply);
 
             double Result1 = n1 - n2;
             Console.WriteLine("the result is " + Result1);
         }
         else if(qp == 3 )
         {
-            Console.WriteLine("Enter the first number to be added ");
-            double n1 = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter the first number to be added ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n1 = double.Parse(reply);
 
 
-            Console.WriteLine("Enter the following number ");
-            double n2 = double.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Enter the following number ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n2 = double.Parse(reply);
 
             double Result1 = n1 * n2;
             Console.WriteLine("the result is " + Result1);
         }
         else if(qp == 4)
         {
-            Console.WriteLine("Enter the first number to be added ");
-            double n1 = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter the first number to be added ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n1 = double.Parse(reply);
 
 
-            Console.WriteLine("Enter the following number ");
-            double n2 = double.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Enter the following number ");
+                reply = Console.ReadLine();
+            }
+            while (!double.TryParse(reply, out n1));
+
+            Convert.ToDouble(reply);
+            n2 = double.Parse(reply);
 
             double Result1 = n1 / n2;
             Console.WriteLine("the result is " + Result1);
